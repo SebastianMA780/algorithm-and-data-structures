253db8c54d8d87458a93ec7e60823ea00e4013d9
Sebastian Marin Arias <sebastianmarin@thepublicgroup.com>
2023-04-15 11:52:04 -0500

    bubble sort algorithm implemented js

---
+++ algorithms/algorithm-complexity/javaScript/big-O/bubbleSort.js
@@ -0,0 +1,33 @@
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+readline.question('What size is the list?: ', (val) => {
+	let value = parseInt(val);
+	let list = Array(value).fill().map(() => Math.round(Math.random() * value))
+
+	console.log(list);
+
+	function bubbleSort(list) {
+		for (let i = 0; i < list.length; i++) {
+			for (let j = 0; j < list.length - i - 1; j++) {
+				if (list[j] > list[j +1]) {
+					let temp = list[j];
+					list[j] = list[j + 1];
+					list[j + 1] = temp;
+				}
+			}
+		}
+
+		return list;
+	}
+
+	console.log(bubbleSort(list));
+	
+  readline.close();
+});
+
+
+
