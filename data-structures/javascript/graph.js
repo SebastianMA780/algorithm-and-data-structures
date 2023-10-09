478ab174be0ae8918f2cd6c67fa7310bb885d8c8
Sebastian Marin Arias <sebastianmarin@thepublicgroup.com>
2023-10-09 09:43:43 -0500

    adds graph exercise

--- data-structures/javascript/graph.js
+++ data-structures/javascript/graph.js
@@ -92,8 +92,12 @@ myGraph.addVertex('5')
 myGraph.addVertex('6')
 myGraph.addVertex('8')
 
+myGraph.addEdge('1','6')
+myGraph.addEdge('6','3')
 myGraph.addEdge('1','3')
 myGraph.addEdge('1','4')
-myGraph.addEdge('1','6')
+myGraph.addEdge('3','5')
+myGraph.addEdge('4','5')
+myGraph.addEdge('8','4')
 
 console.log(myGraph);
