939e182ff96d23450fb6ecdfc3e619d3a7236375
Sebastian Marin Arias <sebastianmarin@thepublicgroup.com>
2023-10-09 09:40:09 -0500

    adds graph exercise

--- data-structures/javascript/graph.js
+++ data-structures/javascript/graph.js
@@ -46,3 +46,54 @@
 		2: [1,1,0,1],
 		3: [0,1,1,0], 
 	}
+
+//Exercise
+// undirected graph
+/* 			
+			   1  -	 6
+			 /	\		/
+	8 - 4			3
+			\		/
+				5
+*/
+
+//  1. Define class and variables we need
+//	2. define methods to add vertex and edges
+//  3. check results
+
+class Graph {
+	constructor() {
+		this.nodes = 0;
+		this.adjacentList = {}
+	}
+
+	addVertex(node) {
+		if(!node) return;
+		if(this.adjacentList[node]) return;
+
+		this.adjacentList[node] = [];
+		this.nodes++;
+	}
+
+	addEdge(node1, node2) {
+		if(!node1 || !node2) return;
+
+		this.adjacentList[node1]?.push(node2);
+		this.adjacentList[node2]?.push(node1);
+
+	}
+}
+
+const myGraph = new Graph();
+myGraph.addVertex('1')
+myGraph.addVertex('3')
+myGraph.addVertex('4')
+myGraph.addVertex('5')
+myGraph.addVertex('6')
+myGraph.addVertex('8')
+
+myGraph.addEdge('1','3')
+myGraph.addEdge('1','4')
+myGraph.addEdge('1','6')
+
+console.log(myGraph);
